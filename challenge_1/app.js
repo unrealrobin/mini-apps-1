c626835ba10702332386a760ea2bbf3ceafff25b
robinnnnnn <rlifshitz19@gmail.com>
2020-06-08 10:24:18 -0700

    Board Created and Can put marks on board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,9 +9,9 @@
       i. Must define all possible 3 in a row combinations on the board
   2. A button that resets the board
     a. grab all the elements on the board and cleat their inner html text
-    b. css grid will make this pretty easy
+    b. css grid will make this pretty easy / may not bee able to use css
   3. on click event add a mark to the board
-    a. after a move is played, run a function that check the board for wins
+    a. after a move is played, run a function that checks the board for wins
     b keep track of move count
       i. if 9 moves have been played and no one wins its a draw
 
@@ -34,20 +34,28 @@
 //if true X otherwise O
 let whichMark = true;
 let mark;
-//logic for mark
-if(whichMark){
-  mark = "X"
-} else {
-  mark = "O";
-}
+let score = 0;
+let board = document.getElementById('board');
 
+
+let getArrayIndexFromElement = () => {
+
+}
 
 //function that adds a mark to a board location
  let playMove = (event) => {
+
+  //logic for mark
+  if(whichMark){
+    mark = "X"
+  } else {
+    mark = "O";
+  }
+
   event.target.innerHTML = mark;
   whichMark = !whichMark;
 
-  //functionality to check for mark
+  //functionality to check for winner
   checkForWinner();
  }
 
@@ -55,4 +63,16 @@ if(whichMark){
  let checkForWinner = () => {
 
  }
+
+ let giveSpotsFunctionality = () => {
+   let allChildren = Array.from(board.children);
+   //console.log(allChildren)
+   allChildren.forEach(child => {
+    child.addEventListener('click', function(event){
+      playMove(event);
+    })
+   })
+ };
+
+ giveSpotsFunctionality();
 
