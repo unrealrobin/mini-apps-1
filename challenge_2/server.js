291fc8512fdaf6e365175bd2279d09ae58f3d034
robinnnnnn <rlifshitz19@gmail.com>
2020-06-09 10:32:58 -0700

    Server Connected / Basic Boiler PLate Implemented

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+//Server Initialization and Responses
 
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.post('/', (req, res) => {
+  res.send('Server is Connected');
+})
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
