70003da3d8ea38ff588db493c5d2e34ea6bf7312
robinnnnnn <rlifshitz19@gmail.com>
2020-06-09 15:29:02 -0700

    Data being passed correctly / Recursive Data CSV Parser Working

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,69 @@
+/*
 
+  1. Submit a JSON object to the input field
+  2. On the server, rearrange JSON obj in a CSV style response
+  3. Return the resonse in the response body
+  4. Append the resonse to the DOM
+
+*/
+
+//function turns json object into  csv style doc
+
+let transform = (jsonObj) => {
+  //return a string of all the keys
+    //return a string of each nested objs properties for all of those keys
+    //use new line symbol '\n'
+  let keys = Object.keys(jsonObj);
+  let arr = [];
+  arr.push(keys.join(','));
+
+
+
+  function recurse(obj){
+    if(obj.children.length == 0){
+      let temp = [];
+        // temp.push('\n');
+      Object.keys(obj).forEach(key => {
+        if(key !== 'children'){
+          temp.push(obj[key]);
+        }
+      })
+      arr.push(temp.join(','));
+      return;
+    }
+
+    if(obj.children.length > 0){
+      //handles parent of children
+      let temp = [];
+        // temp.push('\n');
+      Object.keys(obj).forEach(key => {
+        if(key !== 'children'){
+          temp.push(obj[key]);
+        }
+      })
+      arr.push(temp.join(','));
+
+      //handles children
+      obj.children.forEach(child => {
+        let temp = [];
+        // temp.push('\n');
+        Object.keys(child).forEach(key => {
+          if(key !== 'children'){
+            temp.push(child[key]);
+          }
+      })
+      arr.push(temp.join(','));
+
+      child.children.forEach(child => {
+        recurse(child);
+      })
+      })
+
+
+    }
+  }
+  recurse(jsonObj);
+
+  return arr.join(" \n ");
+}
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,30 @@
 //Server Initialization and Responses
 
 const express = require('express');
+const bodyParser = require('body-parser')
+
+//initialize an express app
 const app = express();
+
+//port to listen on
 const port = 3000;
 
-app.post('/', (req, res) => {
-  res.send('Server is Connected');
+//runs the html page on the server / connects both of them
+app.use(express.static('client'));
+//allows the input type to be a json object
+app.use(bodyParser.urlencoded());
+
+//tell the server where to listen
+app.listen(port, () => console.log(`Listening on port: ${port}`));
+
+//listen to post requests on the endpoint and have access to req body
+app.post('/post', (req, res) => {
+
+  //do some action to the request bosy from a function in app.js
+  let jsonObj = JSON.parse(req.body.jsonInput);
+
+
+  console.log(req.body);
+  res.send(req.body);//server side operations on data
 })
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
